a8d8e8b572b27c2d00ead558d91c26cf26c20e57
ehauptman <esperanzahauptman@gmail.com>
2023-03-18 23:27:45 -0600

    test

---
+++ PriorityQueue.java
@@ -0,0 +1,79 @@
+public class PriorityQueue<E> {
+    Node<E> root;
+//    public String print(Node<E> node){
+//        if(node.right==null) {
+//            return "" +node.value;
+//        }return print(node.right);
+//    }
+    public void enqueue(E value){
+        Node<E> newNode = new Node<>(value);
+        root= merge(root,newNode);
+
+    }
+    private int getNpl(Node<E> t) {
+        if (t == null) return -1;
+        return t.npl;
+    }
+    private void swapkids(Node<E> small){
+        Node<E> temp = small.left;
+        small.left= small.right;
+        small.right= temp;
+    }
+    private void setNullPathLength(Node<E> small){
+        if(small.right==null){
+            small.npl = 1;
+        }else {
+            small.npl = small.right.npl + 1;
+        }
+    }
+    private Node<E> merge(Node<E> t1, Node<E> t2) {
+        Node<E> small;
+        if (t1 == null) return t2;
+        if (t2 == null) return t1;
+        if (t1.compareTo(t2.value) < 0) {
+            t1.right = merge(t1.right, t2);
+            small = t1;
+        } else {
+            t2.right = merge(t2.right, t1);
+            small = t2;
+        }
+        if (getNpl(small.left) < getNpl(small.right))
+            swapkids(small);
+        setNullPathLength(small);
+        return small;
+    }
+    public E dequeue(){
+        E min = root.value;
+        root = merge(root.left,root.right);
+        return min;
+    }
+    public boolean isEmpty(){
+        return root==null;
+    }
+    private class Node<E> implements Comparable<E>{
+
+        Node(E value) {
+            this(value,null, null);
+        }
+
+        public int compareTo(E o) {
+            if(o instanceof Integer){
+                return ((Integer)this.value).compareTo((Integer) o);
+            }else {
+                String temp1 = this.value.toString();
+                String temp2 = o.toString();
+                return temp1.compareTo(temp2);
+            }
+        }
+        Node(E value, Node<E>left, Node<E> right){
+            this.value = value;
+            this.left = left;
+            this.right = right;
+            npl=0;
+        }
+        public E value;
+        public Node<E> left;
+        public Node<E> right;
+        public int npl;
+    }
+}
---
+++ README.md
@@ -0,0 +1,86 @@
+# Assignment 5: Scheduling Priority Queue
+## Introduction:
+I will use leftist priority queues to find the most efficient way to complete a list of tasks.
+## Pseudocode:
+
+### PriorityQueue class:
+    public void enqueue(E value){
+        E min = find min(value, tree root);
+        get right subtree of min until right subtree = null;
+        check S values of right and left child of min ;
+        if(right S> left S){
+            swap right and left child;
+        }
+    }
+    private int getNPL(Node<E> t) {
+        if (t == null) return -1;
+        return t.npl;
+    }
+    public E dequeue(){
+        delete root;
+        merge subtrees of root;
+        return root.value;
+    }
+
+    public boolean isEmpty(){
+        return if(root==null);
+    }
+
+    private class Node<E> { //completed??
+    Node(E value) {
+        this(value,null, null);
+    }
+    Node(E value, Node<E>left, Node<E> right){
+        this.value = value;
+        this.left = left;
+        this.right = right;
+        npl=0;
+    }
+    public E value;
+    public Node<E> left;
+    public Node<E> right;
+    public int npl;
+}
+## Testing:
+    public String print(Node<E> node){
+        if(node.right==null) {
+            return "" +node.value;
+        }return print(node.right);
+    }
+    public String print(Node<E> node){
+        if(node.left==null) {
+            return "" +node.value;
+        }return print(node.right);
+    }
+    public static void simpleQueueTest() {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.enqueue(9);
+        queue.enqueue(7);
+        queue.enqueue(5);
+        queue.enqueue(3);
+        queue.enqueue(1);
+        queue.enqueue(10);
+        while (!queue.isEmpty()) {
+            System.out.println(queue.dequeue());
+        }
+    }
+    Output:
+    1
+    3
+    5
+    7
+    9
+    10
+    public static void simpleQueueTest() {
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        queue.enqueue("chicken");
+        queue.enqueue("lemon");
+        queue.enqueue("apple");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.dequeue());
+        }
+    }
+    Output:
+    apple
+    chicken
+    lemon
